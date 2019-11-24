13f00c9a4513abb3ba06325758298a5b1a3dcc50
takahashi-shinhi-002 <switchscratch015@gmail.com>
2019-11-24 19:19:23 +0900

    ScriptTestプロジェクト 2回目のコミット
    Lesson4の課題提出用のファイルです。

---
+++ Assets/Test.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gundalf
+{
+    int mp = 53;
+
+    public void Magic()
+    {
+        this.mp -= 5;
+
+        if (mp >=5)
+        {
+            Debug.Log("魔法攻撃をした。残りMPは"+ this.mp);
+        }
+        else
+        { 
+            Debug.Log("MPが足りないため魔法が使えない。");
+        }
+            
+    }
+}
+
+public class Test : MonoBehaviour
+{
+
+    int[] array1 = { 10, 20, 30, 40, 50 };
+ 
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i <= 4; i++)
+        {
+            Debug.Log(array1[i]);
+        }
+
+        int[] array2 = { 10, 20, 30, 40, 50 };
+
+        for (int i = 4; i >= 0; i--)
+        {
+            Debug.Log(array2[i]);
+        }
+
+        Gundalf firstmagic = new Gundalf();
+
+        for (int i = 0; i <= 9; i++)
+        {
+            firstmagic.Magic();
+        }
+
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
+ 
